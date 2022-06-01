0f04957e70202759e813df2796686d58d2be07f7
NB2010NB <natanbalai1@gmail.com>
2022-06-01 17:44:58 +0300

    event

---
+++ main.js
@@ -0,0 +1,29 @@
+//*****1******* */
+
+function alertMessage() {
+    alert("wow")
+}
+
+//*******2******* */
+function printLog() {
+    console.log("natan Belay")
+}
+
+//*******3****** */
+function changeColorText() {
+    var colorChange = document.getElementById("changeColor");
+    colorChange.style.color = "green"
+    colorChange.style.fontSize = "90px"
+}
+
+//**********4******** */
+function changeH1Size() {
+    var siz = document.getElementById("changeText");
+    siz.style.fontSize = "90px"
+}
+
+//*********5******* */
+function changePText() {
+    var textP1 = document.getElementById("changeTEXT");
+    textP1.innerText = "natan belay"
+}
